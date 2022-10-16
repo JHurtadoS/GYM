cbff63fe2b8e29bc0235a72a22fbe354c380d813
KarenTovar <112346865+KarenTovar@users.noreply.github.com>
2022-10-16 15:31:33 -0500

    Update UsuarioController.java

--- Controller/UsuarioController.java
+++ Controller/UsuarioController.java
@@ -8,10 +8,40 @@ package Controller;
  *
  * @author FAMILIA TOVAR BUSTOS
  */
-@WebServlet("/UsuarioController")
-public class UsuarioController extends HttpServlet{
+@WebServlet ("/UsuarioController")
+public class UsuarioController extends HttpServlet {
+
     @override 
     protected void doPost(HttpServletRequest request, HttpServletReponse reponse ) {
-        System.out.println("Ingreso al Post");
+        //System.out.println("Ingreso al Post")
+        String doc = request.getParameter("Documento");
+        String nom = request.getParameter("Nombre");
+        String ape = request.getParameter("Apellido");
+        String cel = request.getParameter("Celular");
+        String gen = request.getParameter("Genero");
+        String rh = request.getParameter("Rh");
+
+        /* Imprimir los datos del formulario USUARIO
+        System.out.println("Documento:" + doc);
+        System.out.println("Nombre:" + nom);
+        System.out.println("Apellido:" + ape);
+        System.out.println("Celular:" + cel);
+        System.out.println("Genero:" + gen);
+        System.out.println("Rh:" + rh); */ 
+
+        try {
+            printWriter ou = response.getWriter();
+            ou.print("<html>");
+            ou.print("<head>");
+            ou.print("</head>");
+            ou.print("<body>");
+            ou.print("<h1>");
+            ou.print("DATOS VALLIDOS");
+            ou.print("</h1>");
+            ou.print("</body>");
+            ou.print("</html>");
+        }catch (IOExeption ex) {
+            System.out.println("Exeption" + ex.getMessage());
+        }  
     }
 }
